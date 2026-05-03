ea04e1eac0e54c449d64963a8ed01eeee1242ee3
MarianoE777 <marianoesborraz2020@gmail.com>
2026-05-03 18:39:26 -0300

    agregando el data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const personajes = [
+{
+id: 1,
+nombre: "juan",
+tipo: "caballo naval tactico",
+vida: 1,
+energia: 1,
+nivel: 9999999999999999
+},
+{
+id: 2,
+nombre: "araki",
+tipo: "mangaka",
+vida: 9999999999999,
+energia: 9999999999999,
+nivel: 9999999999999
+},
+{
+id: 3,
+nombre: "cilantro",
+tipo: "araña",
+vida: 900,
+energia: 10000,
+nivel: 1
+}
+];
+const inventarioBase = ["death note", "foto de una piña", "fierro", "sans"];
